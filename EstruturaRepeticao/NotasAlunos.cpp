f80ec436d0a8e77f0c740596c332e1b63a35a3b0
Da0hn <gabrielhondacba@gmail.com>
2022-04-11 11:04:34 -0400

    ajuste no cálculo da nota dos alunos

--- EstruturaRepeticao/NotasAlunos.cpp
+++ EstruturaRepeticao/NotasAlunos.cpp
@@ -1,33 +1,33 @@
 #include <stdio.h>
 //metodo para verificar quais alunos estão aprovados numa escola com a média de 3 notas
 int main(){
+
     int quantidade_de_alunos;
-    // Lê a quantidade de alunos que serão utilizados nos cálculos
+
     printf("informe o numero de alunos: ");
     scanf("%d", &quantidade_de_alunos);
 
-    float notas[quantidade_de_alunos];
+    int quantidade_de_notas = 3;
 
-    // Lê as notas dinamicamente, de acordo com a quantidade de alunos
-    for(int cont = 1; cont <= quantidade_de_alunos; cont++){
-        printf("entre com a nota%d do aluno: ", cont);
-        scanf("%f", &notas[cont]);
+    for(int contador_aluno=1; contador_aluno <= quantidade_de_alunos; contador_aluno++) {
+
+        for(int contador_nota = 1; contador_nota <= quantidade_de_notas; contador_nota++){
+            printf("entre com a nota%d do aluno%d: ", contador_nota, contador_aluno);
+            scanf("%f", &notas[contador_nota]);
+        }
+
+        float acumulador = 0.0;
+        for(int contador_nota=1; contador_nota<= quantidade_de_notas; contador_nota++) {
+            acumulador += notas[cont];
+        }
+
+        float media = acumulador / quantidade_de_notas;
+
+        if(media >= 7)
+            printf("Aluno %d foi APROVADO com media: %.2f\n", contador_aluno, media);
+        else
+            printf("Aluno %d foi REPROVADO com media: %.2f\n", contador_aluno, media);
     }
-
-    // Cálcula a soma das notas lidas
-    float acumulador = 0.0;
-    for(int cont=1; cont <= quantidade_de_alunos; cont++) {
-        acumulador += notas[cont];
-    }
-
-    // Cálcula a média dinamicamente
-    float media = acumulador / quantidade_de_alunos;
-
-    // Decide se o aluno foi ou não aprovado
-    if(media >= 7)
-        printf("Aluno %d foi APROVADO com media: %.2f\n", cont, media);
-    else
-        printf("Aluno %d foi REPROVADO com media: %.2f\n", cont, media);
 
     return 0;
 }
