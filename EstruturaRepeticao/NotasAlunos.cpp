c724cdece9efc3bdd362965ceff0a0bbe5deab58
darlancbmdf <darlan.cbmdf@gmail.com>
2022-04-11 10:05:15 -0300

    correções no código

--- EstruturaRepeticao/NotasAlunos.cpp
+++ EstruturaRepeticao/NotasAlunos.cpp
@@ -4,25 +4,25 @@ int main(){
     float nota1, nota2, nota3, media;
     int num, cont;
     printf("informe o numero de alunos: ");
-    scanf(" %d", &num);
+    scanf("%d", &num);
     nota1 = 0;
     nota2 = 0;
     nota3 = 0;
     media = 0;
     for(cont = 1; cont <= num; cont++){
         printf("entre com a nota1 do aluno: ");
-        scanf(" %f\n", &nota1);
+        scanf("%f", &nota1);
         printf("entre com a nota2 do aluno: ");
-        scanf(" %f\n", &nota2);
+        scanf("%f", &nota2);
         printf("entre com a nota3 do aluno: ");
-        scanf(" %f\n", &nota3);
+        scanf("%f", &nota3);
         
         media = (nota1+nota2+nota3)/3;
 
         if(media >= 7){
-            printf("Aluno %d foi APROVADO com media: %.2f\n", num, media);
+            printf("Aluno %d foi APROVADO com media: %.2f\n", cont, media);
         } else {
-            printf("Aluno %d foi REPROVADO com media: %.2f\n", num, media);
+            printf("Aluno %d foi REPROVADO com media: %.2f\n", cont, media);
         }
     }    
 }
